17b3893eaae46c41d21e76e3256541b2fc655110
Josh Dey <josh.dey@gmail.com>
2019-11-14 03:40:15 -0800

    Update auxiliary.py

--- Code/auxiliary.py
+++ Code/auxiliary.py
@@ -67,3 +67,45 @@ def RMS_over_t(act,exp):
     rms=np.sqrt(resid/n)
 
     return rms
+
+def fir_downsample(data,new_dt,old_dt):
+    """
+    Downsamples data for use in ESNs. Useful if teacher data is sampled at a
+    small dt, but less accuracy is wanted for feeding to the ESN.
+
+    data should be a 1D numpy array.
+    """
+    resamp=int(new_dt/old_dt)
+    new_data=sps.decimate(data,resamp,ftype='fir',axis=0)
+    return new_data
+
+def fft_downsample(data,new_dt,old_dt):
+    old_len=data.shape[0]
+    resamp=new_dt/old_dt
+    new_len=int(old_len/resamp)
+    new_data=sps.resample(data,new_len)
+    return new_data
+
+def taylor_exp(x_in,coeffs):
+    dim=coeffs.shape[0]
+    x_out=0
+    for i in range(0,dim):
+        x_out+=coeffs[i]*np.power(x_in,i)
+    return x_out
+
+def ReLU(x_in,coeffs):
+    scaled=coeffs*x_in
+    x_out=np.maximum(scaled,0)
+    return x_out
+
+def autocorrelation(x,norm=False):
+    l=x.shape[0]
+    x=x-np.mean(x)
+    var=np.var(x)
+    try:
+        corr=np.correlate(x,x,mode='full')[-l:]
+    except:
+        raise IndexError('Array was possibly not 1D')
+    #corr=corr[int(corr.size/2):]
+    res=corr/(var*(np.arange(l,0,-1)))
+    return res
