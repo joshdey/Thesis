131ff44a8820663e8ccb604d57e4e23f621b25d7
Josh Dey <josh.dey@gmail.com>
2020-02-19 18:36:45 -0800

    added RMS and RMS/t functions

--- Code/ESN.py
+++ Code/ESN.py
@@ -535,3 +535,41 @@ def load_ESN(file):
     ESN.W_out=mats['Wout']
 
     return ESN
+
+def RMS(act,exp):
+    """
+    act: actual generated values
+    exp: expected values
+
+    Both inputs should be numpy arrays
+    """
+    act=act.reshape((act.shape[0],1))
+    exp=exp.reshape((exp.shape[0],1))
+    try:
+        resid=np.square(act-exp)
+    except:
+        raise IndexError("Indices of act and exp do not match")
+    resid=np.sum(resid)
+    n=act.shape[0]
+    rms=mt.sqrt(resid/n)
+
+    return rms
+
+def RMS_over_t(act,exp):
+    """
+    Returns the RMS at each time step for many time steps
+
+    act:(time,vals) array of generated values
+    exp:(time,vals) array of expected values
+
+    returns: (time, RMS) array of rms values
+    """
+    try:
+        resid=np.square(act-exp)
+    except:
+        raise IndexError("Indices of act and exp probably do not match")
+    resid=np.sum(resid,axis=1)
+    n=act.shape[1]
+    rms=np.sqrt(resid/n)
+
+    return rms
