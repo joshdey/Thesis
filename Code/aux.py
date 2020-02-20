057c6eff8089c0d12e9b7d5bec5cebc097a5c48f
Josh Dey <josh.dey@gmail.com>
2020-02-20 12:49:19 -0800

    updated wrapper

--- Code/aux.py
+++ Code/aux.py
@@ -59,9 +59,9 @@ def ReLU(x_in,coeffs):
     x_out=np.maximum(scaled,0)
     return x_out
 
-def ESN_wrapper(teach_in,teach_out,run,N=400,fb=0,rho=1,W_sig=1,in_sig=1,fb_sig=1,
-                dens=0.05,in_dens=0.8,fb_dens=0.1,a=1,noise=0.0,bias=1,bin_node=0,
-                wo=100,MP=True,B=0.1,around=0,order=None,directed=True,name=None):
+def ESN_wrapper(teach_in,teach_out,run,N=400,fb=0,rho=1,W_sig=1,in_sig=.25,fb_sig=1,
+                dens=0.25,in_dens=0.8,fb_dens=0.1,a=1,noise=0.0,bias=0,bin_node=0,
+                wo=100,MP=True,B=0.1,around=0,order=None,directed=True):
     """
     Wrapper for initializing, training, and running an ESN. Nice for parameter
     searches/generating large data sets for performance comparison. Returns
@@ -81,7 +81,65 @@ def ESN_wrapper(teach_in,teach_out,run,N=400,fb=0,rho=1,W_sig=1,in_sig=1,fb_sig=
                   bias=bias,mp=MP,B=B)
     run_dims=run.shape
     ESN.run_ESN(input_dat=run,around=around,order=order,time=run_dims[0],init='last')
+    plot=ESN.plot_attractor()
 
     out=ESN.outputs
-    plot=ESN.plot_attractor(name=name)
-    return out,plot
+    return out
+
+def esn_wrapper(N=400,fb=0,rho=1,new_rho=1,W_sig=1,in_sig=.25, fb_sig=1,
+                dens=0.25,in_dens=0.8,fb_dens=0,a=1,noise=0.0,bias=0,
+                bin_node=0,wo=100,MP=True,B=0.1,around=1,order=1,directed=True,
+                teacher_system=None, directions=0,train_time=400,
+                run_time=30000, dt=0.01):
+    """
+    wrapper for initializing, training, and running the ESN.
+    Inputs: ESN parameters, teacher input, teacher output, and running input.
+    Output(s): TBD
+    """
+    if teacher_system=='Rossler':
+        xteach, yteach, zteach=ESNteachers.Rossler(l=train_time, dt=dt)
+        fullrun=ESNteachers.Rossler(l=run_time,dt=dt)[directions]
+        run=fullrun[directions]
+    else:
+        xteach, yteach, zteach=ESNteachers.Lorenz(l=train_time, dt=dt)
+        fullrun=ESNteachers.Lorenz(l=run_time, dt=dt)
+        run=fullrun[directions]
+    if directions==0:
+        teach_out = np.concatenate((yteach,zteach), axis=1)
+        teach_in=xteach
+        act1=fullrun[1]
+        act2=fullrun[2]
+    elif directions==1:
+        teach_out = np.concatenate((xteach,zteach), axis=1)
+        teach_in=yteach
+        act1=fullrun[0]
+        act2=fullrun[2]
+    else:
+        teach_out = np.concatenate((xteach,yteach), axis=1)
+        teach_in=zteach
+        act1=fullrun[0]
+        act2=fullrun[1]
+
+    in_dims=teach_in.shape
+    out_dims=teach_out.shape
+
+    ESN=esn.simple_ESN(N=N, K=in_dims[1], L=out_dims[1], a=a,
+                        binary_node=bin_node, feedback=fb, W_sig=W_sig,
+                        in_sig=in_sig, fb_sig=fb_sig, directed)
+    ESN.generate_W(rho=rho, dens=dens)
+    ESN.rescale_W(new_rho=new_rho)
+    ESN.generate_Win(dens=in_dens)
+    if fb==1:
+        ESN.generate_Wfb(dens=fb_dens)
+    ESN.train_ESN(input_dat=teach_in,teacher=teach_out,around=around,
+                    order=order,washout=wo,noise=noise,bias=bias,mp=MP,B=B)
+    run_dims=run.shape
+    ESN.run_ESN(input_dat=run,around=around,order=order,time=run_dims[0],
+                init='last')
+    out=ESN.outputs
+    run1, run2=np.hsplit(out, 2)
+
+    rms=[aux.RMS(run1, act1), aux.RMS(run2, act2)]
+    rmse1=aux.RMS_over_t(run1, act1)
+    rmse2=aux.RMS_over_t(run2, act2)
+    return out, rms, rmse1, rmse2
