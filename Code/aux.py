383d425a1c2932fdf171c1a1dc98eb8d6806993a
Josh Dey <josh.dey@gmail.com>
2020-02-20 13:46:40 -0800

    return both outputs separately

--- Code/aux.py
+++ Code/aux.py
@@ -144,4 +144,4 @@ def esn_wrapper(N=400,fb=0,rho=1,new_rho=1,W_sig=1,in_sig=.25, fb_sig=1,
     rmse1=RMS_over_t(run1, act1)
     rmse2=RMS_over_t(run2, act2)
     plot=ESN.plot_attractor()
-    return out, rms, rmse1, rmse2, plot
+    return run1, run2, rms, rmse1, rmse2, plot
